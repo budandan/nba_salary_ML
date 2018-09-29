b99ddefc052c0638b4e587e3b0ce6f65cf613391
Daniel Ferolino <danielferolino@Daniels-MBP.app.byu.edu>
2018-09-28 21:21:21 -0600

    blahhh

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
     require($_SERVER["DOCUMENT_ROOT"]."/php/connect.php");
-    require($_SERVER["DOCUMENT_ROOT"]."/open_body.php");
-    require($_SERVER["DOCUMENT_ROOT"]."/navbar.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/php/open_body.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/php/navbar.php");
 ?>
+
+<!-- CODE HERE -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            SUUUUU DUDE
+        </div>
+    </div>
+</div>
 
 <?php // CODE HERE
     $select_all_query = "SELECT * FROM player;";
@@ -14,5 +24,5 @@
 ?>
 
 <?php
-    require($_SERVER["DOCUMENT_ROOT"]."/close_body.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/php/close_body.php");
 ?>
