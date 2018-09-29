c138bbb952c1d3fc99bae2c24732c467bf601a3b
Daniel Ferolino <danielferolino@Daniels-MBP.app.byu.edu>
2018-09-28 21:15:35 -0600

    modularized

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        require($_SERVER["DOCUMENT_ROOT"]."/php/connect.php");
-        $select_all_query = "SELECT * FROM player;";
-        if ($players = mysqli_query($con, $select_all_query)) {
-            while ($row = mysqli_fetch_assoc($players)) {
-                echo $row['name'] . ' has id: ' . $row['id'];
-            }
+<?php
+    require($_SERVER["DOCUMENT_ROOT"]."/php/connect.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/open_body.php");
+    require($_SERVER["DOCUMENT_ROOT"]."/navbar.php");
+?>
+
+<?php // CODE HERE
+    $select_all_query = "SELECT * FROM player;";
+    if ($players = mysqli_query($con, $select_all_query)) {
+        while ($row = mysqli_fetch_assoc($players)) {
+            echo $row['name'] . ' has id: ' . $row['id'];
         }
-    ?>
-</body>
-</html>
+    }
+?>
+
+<?php
+    require($_SERVER["DOCUMENT_ROOT"]."/close_body.php");
+?>
